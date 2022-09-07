57802f9c13de44232d125ecf92e24a1e5d113fed
AlexisFleming <s223250899@mandela.ac.za>
2022-09-07 11:26:38 +0200

    <locallexi rebase>

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Final_LitchiLearn.Models;
 
 namespace Final_LitchiLearn.Controllers
 {
     public class TeacherController : Controller
     {
+        //[Authorize(Roles = "Teacher")]
         public IActionResult Index()
         {
             return View();
         }
+        
+        public IActionResult Subject()
+        {
+            return View();
+        }
+        public IActionResult Topics()
+        {
+            return View();
+        }
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+        public IActionResult Student()
+        {
+            return View();
+        }
+        public IActionResult QuizMarks()
+        {
+            return View();
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+     
+
     }
 }
