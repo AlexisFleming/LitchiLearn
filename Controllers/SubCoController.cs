e0dfa31450a1eb32000f069172b5b8d9f461880d
BrandSandle <s220013489@mandela.ac.za>
2022-09-09 09:51:26 +0200

    added SubCo

---
+++ Controllers/SubCoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_LitchiLearn.Controllers
+{
+    public class SubCoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+        public IActionResult Moderate()
+        {
+            return View();
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
